c7ea21778915dd0944c3bd95fab50c75be998e21
Keith Glines <keithglines@yahoo.com>
2022-06-04 15:57:17 -0400

    Phase 2

--- server/app.js
+++ server/app.js
@@ -5,6 +5,15 @@ require('express-async-errors');
 app.use('/static', express.static('assets'));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  console.log(res.statusCode);
+  res.on("finish", () => {
+    console.log(res.statusCode);
+  })
+  next();
+})
+
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
@@ -22,6 +31,12 @@ app.post('/test-json', (req, res, next) => {
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
+
+app.use((req, res, next) => {
+  res.status(404);
+  let error = new Error("The requested resource couldn't be found.")
+  res.json(error.message);
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
